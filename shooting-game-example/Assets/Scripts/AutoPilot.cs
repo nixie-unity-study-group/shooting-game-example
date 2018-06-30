16bdd1667629189af8c1b8615fdd311481c3b595
Suyeong Rhie <noxowl@creamcrepe.local>
2018-06-30 13:17:08 +0900

    add autopilot

--- shooting-game-example/Assets/Scripts/AutoPilot.cs
+++ shooting-game-example/Assets/Scripts/AutoPilot.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoPilot : MonoBehaviour {
+	private bool isMoveRight = true;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (transform.position.x < 5.0f & isMoveRight) {
+			Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+			Vector2 nextPosition = new Vector2(5, 0);
+			transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
+		} else if (transform.position.x >= 5.0f & isMoveRight) {
+			isMoveRight = false;
+		} else if (transform.position.x <= -5.0f & !isMoveRight) {
+			isMoveRight = true;
+		} else if (transform.position.x > -5.0f & !isMoveRight) {
+		    Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+			Vector2 nextPosition = new Vector2(-5, 0);
+			transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);	
+		}
+		
+
+		
 		
 	}
 }
